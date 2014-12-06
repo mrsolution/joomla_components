f6b241bcdafbc595154f1ba5cf360b16e00aaab5
gani <gani@tasolglobal.com>
2014-12-06 17:18:30 +0530

    helloworld and category component

---
+++ com_helloworld/admin/sql/install.mysql.utf8.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `#__helloworld`;
+
+CREATE TABLE `#__helloworld` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `greeting` varchar(255) NOT NULL,
+  `published` tinyint(2) NOT NULL,
+  `checked_out` int(11) NOT NULL,
+  `checked_out_time` datetime NOT NULL,
+  `created_by` int(11) NOT NULL,
+   PRIMARY KEY  (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+INSERT INTO `#__helloworld` VALUES(0,'Hello World',1,0,0,0);
